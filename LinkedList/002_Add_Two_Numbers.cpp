a9375733a8b92eba633d9a446c2f59b2a7fe30bc
sk52100 <sr5210095@gmail.com>
2026-02-09 21:56:19 +0530

    LC #2 Add Two Numbers

---
+++ LinkedList/002_Add_Two_Numbers.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list node.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+
+        return dummy->next;
+    }
+};
